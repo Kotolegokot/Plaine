75955faf08123a334231b716ed28f97acd4daf22
Kotolegokot <gkotolegokot@gmail.com>
2016-09-26 00:27:53 +0300

    Add script to compile localization files

---
+++ compile_localization.sh
@@ -0,0 +1,5 @@
+#!/bin/sh
+
+msgfmt media/locale/ru/LC_MESSAGES/planerunner.po -o media/locale/ru/LC_MESSAGES/planerunner.mo
+msgfmt media/locale/crh/LC_MESSAGES/planerunner.po -o media/locale/crh/LC_MESSAGES/planerunner.mo
+msgfmt media/locale/crh@cyrillic/LC_MESSAGES/planerunner.po -o media/locale/crh@cyrillic/LC_MESSAGES/planerunner.mo
