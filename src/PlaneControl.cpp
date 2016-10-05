3e2a9c7948383614b77adafafa32397913a79f27
XnekrosX <xxxnekrosxxx@gmail.com>
2016-10-05 18:07:32 +0300

    Change realization of turning

--- src/PlaneControl.cpp
+++ src/PlaneControl.cpp
@@ -6,18 +6,19 @@ PlaneControl::PlaneControl(Plane *plane, const Controls &controls) :
 // move the plane according to keys pressed
 void PlaneControl::handle(EventReceiver *eventReceiver)
 {
-    btVector3 turnForce(0, 0, 1);
+    btVector3 turnImpulse(0, 0, 0);
 
     // up and down
     if (!(eventReceiver->IsKeyDown(controls.up) &&
         eventReceiver->IsKeyDown(controls.down))) { // if not both are pressed
         // up
         if (eventReceiver->IsKeyDown(controls.up)) {
-            turnForce += btVector3(0, 50, 0);
+            turnImpulse = btVector3(0, 1, 0);
+
         }
         // down
         else if (eventReceiver->IsKeyDown(controls.down)) {
-            turnForce += btVector3(0, -50, 0);
+            turnImpulse = btVector3(0, -1, 0);
         }
     }
 
@@ -26,11 +27,11 @@ void PlaneControl::handle(EventReceiver *eventReceiver)
         eventReceiver->IsKeyDown(controls.right))) { // if not both are pressed
         // left
         if (eventReceiver->IsKeyDown(controls.left)) {
-            turnForce += btVector3(-50, 0, 0);
+            turnImpulse = btVector3(-1, 0, 0);
         }
         // right
         else if (eventReceiver->IsKeyDown(controls.right)) {
-            turnForce += btVector3(50, 0, 0);
+            turnImpulse = btVector3(1, 0, 0);
         }
     }
 
@@ -41,10 +42,8 @@ void PlaneControl::handle(EventReceiver *eventReceiver)
     planeVelosity *= 0.001f*planeVelocityLength*planeVelocityLength;
     plane->getRigidBody()->applyForce(planeVelosity, btVector3(0, 0, 0));
 
-    turnForce.normalize();
-    turnForce *= 1000;
-    plane->getRigidBody()->applyForce(turnForce, btVector3(0, 0, 0));
-    plane->getRigidBody()->applyForce(btVector3(0, 0, 300), btVector3(0, 0, 0));
+    turnImpulse *= 25;
+    plane->getRigidBody()->applyCentralImpulse(turnImpulse);
 }
 
 void PlaneControl::setPlane(Plane *plane)
