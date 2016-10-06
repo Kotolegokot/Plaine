f8137646758b4c019e4b582f11eb0b316567f4ac
Kotolegokot <gkotolegokot@gmail.com>
2016-10-06 03:11:25 +0300

    Update count_lines.sh

--- count_lines.sh
+++ count_lines.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-find include src . -maxdepth 1 | grep -P '.*\.(cpp|h)' | grep  -v 'CGUITTFont' | xargs wc -l
+find include src include/obstacles src/obstacles . -maxdepth 1 | grep -P '.*\.(cpp|h)' | grep  -v 'CGUITTFont' | xargs wc -l
