7a4d931e47a1acb0d619b00173b680a59bc05fc8
Kotolegokot <gkotolegokot@gmail.com>
2016-10-13 20:58:44 +0300

    super_script.sh can show the list of all c++ files

--- super_script.sh
+++ super_script.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 function show_usage {
-	echo "Usage: $0 (count_lines | update_l11n | compile_l11n | add_license_headers)"
+	echo "Usage: $0 ( ls | count_lines | update_l11n | compile_l11n | add_license_headers | replace_license_headers)"
 	exit
 }
 
@@ -11,7 +11,11 @@ fi
 
 CXX_FILES=`find include/ src/ | grep -P '.*\.(cpp|h)$' | grep -v 'CGUITTFont'`\ main.cpp
 
-if [ "$1" == "count_lines" ]; then
+if [ "$1" == "ls" ]; then
+	for file in $CXX_FILES; do
+		echo $file
+	done
+elif [ "$1" == "count_lines" ]; then
 	wc -l $CXX_FILES
 elif [ "$1" == "update_l11n" ]; then
 	xgettext -k_wp -k_w -k_ $CXX_FILES -o planerunner.pot
