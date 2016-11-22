88427c0ed26f217ab88984fd3e6fc97c422555c3
Kotolegokot <gkotolegokot@gmail.com>
2016-11-22 19:52:52 +0300

    Fix Alley

--- include/patterns/Alley.h
+++ include/patterns/Alley.h
@@ -26,29 +26,33 @@ public:
 
         btVector3 position { 1.5f * CELL_LENGTH, 1.5f * CELL_LENGTH, 0 };
 
-        constexpr btScalar cos45 = 0.70710678118;
-        constexpr btScalar edge = 150;
+        static constexpr btScalar cos45 = 0.70710678118;
+        static constexpr btScalar edge = 150;
 
         for (std::size_t i = 0; i < Length; i++) {
             result.push_back(std::make_unique<IcosahedronProducer>(edge));
             result.back()->relativeTransform.setOrigin(position + (i % 2 == 0 ?
-                        btVector3(-CELL_LENGTH, 0, CELL_LENGTH * i * 2) :
-                        btVector3(-CELL_LENGTH * cos45, CELL_LENGTH * cos45, CELL_LENGTH * i * 2)));
+                          btVector3(-CELL_LENGTH, 0, CELL_LENGTH * i * 2) :
+                          btVector3(-CELL_LENGTH * cos45, CELL_LENGTH * cos45,
+                                    CELL_LENGTH * i * 2)));
 
             result.push_back(std::make_unique<IcosahedronProducer>(edge));
             result.back()->relativeTransform.setOrigin(position + (i % 2 == 0 ?
-                        btVector3(CELL_LENGTH, 0, CELL_LENGTH * i * 2) :
-                        btVector3(CELL_LENGTH * cos45, -CELL_LENGTH * cos45, CELL_LENGTH * i * 2)));
+                          btVector3(CELL_LENGTH, 0, CELL_LENGTH * i * 2) :
+                          btVector3(CELL_LENGTH * cos45, -CELL_LENGTH * cos45,
+                                    CELL_LENGTH * i * 2)));
 
             result.push_back(std::make_unique<IcosahedronProducer>(edge));
             result.back()->relativeTransform.setOrigin(position + (i % 2 == 0 ?
-                        btVector3(0, -CELL_LENGTH, CELL_LENGTH * i * 2) :
-                        btVector3(-CELL_LENGTH * cos45, -CELL_LENGTH * cos45, CELL_LENGTH * i * 2)));
+                          btVector3(0, -CELL_LENGTH, CELL_LENGTH * i * 2) :
+                          btVector3(-CELL_LENGTH * cos45, -CELL_LENGTH * cos45,
+                                    CELL_LENGTH * i * 2)));
 
             result.push_back(std::make_unique<IcosahedronProducer>(edge));
-            result.back()->relativeTransform(position + (i % 2 == 0 ?
-                        btVector3(0, CELL_LENGTH, CELL_LENGTH * i * 2) :
-                        btVector3(CELL_LENGTH * cos45, CELL_LENGTH * cos45, CELL_LENGTH * i * 2)));
+            result.back()->relativeTransform.setOrigin(position + (i % 2 == 0 ?
+                          btVector3(0, CELL_LENGTH, CELL_LENGTH * i * 2) :
+                          btVector3(CELL_LENGTH * cos45, CELL_LENGTH * cos45,
+                                    CELL_LENGTH * i * 2)));
         }
 
         return result;
