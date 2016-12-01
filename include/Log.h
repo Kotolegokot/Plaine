4dfbcb60c8bc28405baf945e6b4038161d1ba417
Kotolegokot <gkotolegokot@gmail.com>
2016-12-01 08:22:47 +0300

    Add wchar_t support to Log

--- include/Log.h
+++ include/Log.h
@@ -12,9 +12,11 @@ const std::string LOG_FILE { "logfile" };
 enum class severity_level { error, warning, notice, info, debug };
 
 std::ostream &operator <<(std::ostream &out, severity_level level);
+std::wostream &operator <<(std::wostream &wout, severity_level level);
 
 class Log {
     static std::ofstream out;
+    static std::wofstream wout;
     static std::mutex mutex;
 
 public:
@@ -30,9 +32,24 @@ public:
     }
 
     template <typename... Args>
+    static void wwrite(severity_level level, Args &&... args)
+    {
+        std::lock_guard<std::mutex> lock(mutex);
+        wout << level << L" ";
+        (void)(int []){ 0, (out << std::forward<Args>(args), 0)... };
+        wout << std::endl;
+    }
+
+    template <typename... Args>
     static void error(Args &&... args)
     {
         write(severity_level::error, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    static void werror(Args &&... args)
+    {
+        wwrite(severity_level::error, std::forward<Args>(args)...);
     }
 
     template <typename... Args>
@@ -42,9 +59,21 @@ public:
     }
 
     template <typename... Args>
+    static void wwarning(Args &&... args)
+    {
+        wwrite(severity_level::warning, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
     static void notice(Args &&... args)
     {
         write(severity_level::notice, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    static void wnotice(Args &&... args)
+    {
+        wwrite(severity_level::notice, std::forward<Args>(args)...);
     }
 
     template <typename... Args>
@@ -54,9 +83,21 @@ public:
     }
 
     template <typename... Args>
+    static void winfo(Args &&... args)
+    {
+        wwrite(severity_level::info, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
     static void debug(Args &&... args)
     {
         write(severity_level::debug, std::forward<Args>(args)...);
+    }
+
+    template <typename... Args>
+    static void wdebug(Args &&... args)
+    {
+        wwrite(severity_level::debug, std::forward<Args>(args)...);
     }
 };
 
--- src/Log.cpp
+++ src/Log.cpp
@@ -1,6 +1,7 @@
 #include "Log.h"
 
 std::ofstream Log::out { LOG_FILE };
+std::wofstream Log::wout { LOG_FILE };
 std::mutex Log::mutex;
 
 std::ostream &operator <<(std::ostream &out, severity_level level)
@@ -19,4 +20,22 @@ std::ostream &operator <<(std::ostream &out, severity_level level)
     }
 
     return out;
+}
+
+std::wostream &operator <<(std::wostream &wout, severity_level level)
+{
+    switch (level) {
+    case severity_level::error:
+        wout << L"ERROR   :";
+    case severity_level::warning:
+        wout << L"WARNING :";
+    case severity_level::notice:
+        wout << L"NOTICE  :";
+    case severity_level::info:
+        wout << L"INFO    :";
+    case severity_level::debug:
+        wout << L"DEBUG   :";
+    }
+
+    return wout;
 }
