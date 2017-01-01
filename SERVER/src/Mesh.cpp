916d12b564028152ae3412ca110778a19a83b169
Kotolegokot <gkotolegokot@gmail.com>
2017-01-01 18:51:16 +0300

    Add Mesh class

---
+++ SERVER/src/Mesh.cpp
@@ -0,0 +1,309 @@
+/* This file is part of Plaine.
+ *
+ * Plaine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Plaine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Plaine. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "Mesh.hpp"
+
+Mesh::Mesh(const std::string &filename, btScalar scale)
+{
+    load(filename, scale);
+}
+
+std::vector<Mesh::Item> Mesh::parse(const std::string &filename)
+{
+    std::ifstream input(filename);
+    if (!input.is_open())
+        return std::vector<Item>();
+
+    std::vector<Item> items;
+    std::string str;
+    int back = 0;
+
+    enum { NONE, INT, FLOAT, KEYWORD, STRING, SPACE_BEFORE_STRING, COMMENT, SPACE_BEFORE_COMMENT } state = NONE;
+    while (input) {
+        int c = (back ? back : input.get());
+        back = 0;
+        switch (state) {
+        case NONE:
+            if (c == '\n') {
+                items.push_back(Item(Item::NEWLINE));
+            } else if (isspace(c)) {
+                // nothing
+            } else if (c == '/') {
+                items.push_back(Item(Item::SLASH));
+            } else if (c == '-' || isdigit(c)) {
+                str.push_back(c);
+                state = INT;
+            } else if (isalpha(c)) {
+                str.push_back(c);
+                state = KEYWORD;
+            } else if (c == '#') {
+                state = SPACE_BEFORE_COMMENT;
+            } else if (c == EOF) {
+                break;
+            } else
+                throw ParseError("unexpected symbol: '" + std::string(c, 1) + "'");
+            break;
+
+        case INT:
+            if (isdigit(c)) {
+                str.push_back(c);
+            } else if (c == '.') {
+                str.append(localeconv()->decimal_point);
+                state = FLOAT;
+            } else {
+                back = c;
+                items.push_back(Item(Item::INT, std::stoi(str)));
+                str.clear();
+                state = NONE;
+            }
+            break;
+
+        case FLOAT:
+            if (isdigit(c)) {
+                str.push_back(c);
+            } else {
+                back = c;
+                items.push_back(Item(Item::FLOAT, std::stof(str)));
+                str.clear();
+                state = NONE;
+            }
+            break;
+
+        case KEYWORD:
+            if (isalnum(c)) {
+                str.push_back(c);
+            } else {
+                items.push_back(Item(Item::KEYWORD, str));
+                state = (str == "mtllib" || str == "usemtl" || str == "o" || str == "g")
+                    ? SPACE_BEFORE_STRING : NONE;
+                back = c;
+                str.clear();
+            }
+            break;
+
+        case SPACE_BEFORE_STRING:
+            if (!isspace(c)) {
+                back = c;
+                state = STRING;
+            }
+            break;
+
+        case STRING:
+            if (c == '\n' || c == EOF) {
+                back = c;
+                items.push_back(Item(Item::STRING, str));
+                str.clear();
+                state = NONE;
+            } else {
+                str.push_back(c);
+            }
+            break;
+
+        case SPACE_BEFORE_COMMENT:
+            if (!isspace(c)) {
+                back = c;
+                state = COMMENT;
+            }
+            break;
+
+        case COMMENT:
+            if (c == '\n' || c == EOF) {
+                back = c;
+                items.push_back(Item(Item::COMMENT, str));
+                str.clear();
+                state = NONE;
+            } else {
+                str.push_back(c);
+            }
+        }
+    }
+
+    if (items.back().type != Item::NEWLINE)
+        items.push_back(Item(Item::NEWLINE));
+
+    return items;
+}
+
+void Mesh::warning(Item::Type expected, Item::Type found)
+{
+    throw ParseError(Item::show_type(expected) + " expected, but " +
+                     Item::show_type(found) + " found");
+}
+
+#define \
+EXPECT(_expected) {\
+    if ((*i).type != _expected) {\
+        warning(_expected, (*i).type);\
+        goToNextNEWLINE = true;\
+        state = NONE;\
+        break;\
+    }\
+}
+
+void Mesh::load(const std::string &filename, btScalar scale)
+{
+    vertices.clear();
+    polygons.clear();
+
+    std::vector<Item> items = parse(filename);
+
+    /*for (const Item &item : items) {
+        if (item.type == Item::SLASH)
+            std::cout << "SLASH" << std::endl;
+        else if (item.type == Item::INT)
+            std::cout << "INT: " << (int) item.getInt() << std::endl;
+        else if (item.type == Item::FLOAT)
+            std::cout << "FLOAT: " << (float) item.getFloat() << std::endl;
+        else if (item.type == Item::STRING)
+            std::cout << "STRING: '" << item.getString() << "'" << std::endl;
+        else if (item.type == Item::COMMENT)
+            std::cout << "COMMENT: '" << item.getString() << "'" << std::endl;
+        else if (item.type == Item::KEYWORD)
+            std::cout << "KEYWORD: " << item.getString() << std::endl;
+        else if (item.type == Item::NEWLINE)
+            std::cout << "NEWLINE" << std::endl;
+    }*/
+
+    bool goToNextNEWLINE = false;
+    enum { NONE, VERTEX, FACE } state = NONE;
+
+    for (std::vector<Item>::const_iterator i = items.cbegin(); i != items.cend(); ++i) {
+        if (goToNextNEWLINE) {
+            if (i->type == Item::NEWLINE)
+                goToNextNEWLINE = false;
+        } else {
+            switch (state) {
+            case NONE:
+                if (i->type == Item::COMMENT) {
+                    goToNextNEWLINE = true;
+                    break;
+                }
+                EXPECT(Item::KEYWORD);
+
+                if (i->getString() == "v") {
+                    state = VERTEX;
+                } else if (i->getString() == "f") {
+                    state = FACE;
+                } else {
+                    goToNextNEWLINE = true;
+                }
+                break;
+
+            case VERTEX:
+                {
+                    btVector3 vertex;
+
+                    EXPECT(Item::FLOAT);
+                    vertex.setX(-i->getFloat());
+                    ++i;
+
+                    EXPECT(Item::FLOAT);
+                    vertex.setY(i->getFloat());
+                    ++i;
+
+                    EXPECT(Item::FLOAT);
+                    vertex.setZ(i->getFloat());
+                    ++i;
+
+                    if (i->type == Item::FLOAT)
+                        ++i;
+                    EXPECT(Item::NEWLINE);
+
+                    vertices.push_back(vertex * scale);
+
+                    state = NONE;
+                }
+                break;
+
+            case FACE:
+                {
+                    std::vector<std::size_t> polygon;
+
+                    while (i->type != Item::NEWLINE) {
+                        EXPECT(Item::INT);
+                        polygon.push_back(i->getInt() - 1);
+                        ++i;
+
+                        if (i->type == Item::SLASH) {
+                            ++i;
+                            if (i->type == Item::SLASH) {
+                                ++i;
+                                EXPECT(Item::INT);
+                                ++i;
+                            } else if (i->type == Item::INT) {
+                                ++i;
+                                if (i->type == Item::SLASH) {
+                                    ++i;
+                                    EXPECT(Item::INT);
+                                    ++i;
+                                }
+                            } else {
+                                throw ParseError(Item::show_type(Item::SLASH) + " or " +
+                                                 Item::show_type(Item::INT) + " expected, but " +
+                                                 Item::show_type(i->type) + " found");
+                                state = NONE;
+                                goToNextNEWLINE = true;
+                                break;
+                            }
+                        }
+                    }
+
+                    polygons.push_back(polygon);
+
+                    state = NONE;
+                }
+                break;
+            }
+        }
+    }
+
+    /*std::ofstream out(filename + "_");;
+    for (std::vector<btVector3>::const_iterator i = vertices.cbegin(); i != vertices.cend(); i++)
+        out << "v " << i->x() << " " << i->y() << " " << i->z() << std::endl;
+
+    for (std::vector<std::vector<std::size_t>>::const_iterator i = polygons.cbegin(); i != polygons.cend(); i++) {
+        out << "f ";
+        for (std::vector<std::size_t>::const_iterator j = i->cbegin(); j != i->cend(); j++)
+            out << *j << " ";
+        out << std::endl;
+    }*/
+}
+
+std::unique_ptr<btTriangleMesh> Mesh::triangleMesh() const
+{
+    auto triangleMesh = std::make_unique<btTriangleMesh>();
+    for (const std::vector<std::size_t> &polygon : polygons)
+        for (std::size_t i = 1; i < polygon.size() - 1; i++)
+            triangleMesh->addTriangle(vertices[polygon[0]], vertices[polygon[i]], vertices[polygon[i + 1]]);
+
+    return triangleMesh;
+}
+
+btVector3 *Mesh::points()
+{
+    return vertices.data();
+}
+
+std::size_t Mesh::points_count() const
+{
+    return vertices.size();
+}
+
+void Mesh::set_points(btConvexHullShape &shape)
+{
+    for (const btVector3 &vertex : vertices)
+        shape.addPoint(vertex);
+}
---
+++ SERVER/src/Mesh.hpp
@@ -0,0 +1,153 @@
+/* This file is part of Plaine.
+ *
+ * Plaine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Plaine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Plaine. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <iostream>
+#include <memory>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <sstream>
+#include <btBulletDynamicsCommon.h>
+
+#include "ParseError.hpp"
+
+class Mesh
+{
+public:
+    Mesh() = default;
+    Mesh(const std::string &filename, btScalar scale = 1.0f);
+    void load(const std::string &filename, btScalar scale = 1.0f);
+    std::unique_ptr<btTriangleMesh> triangleMesh() const;
+    btVector3 *points();
+    std::size_t points_count() const;
+    void set_points(btConvexHullShape &shape);
+
+private:
+    std::vector<btVector3> vertices;
+    std::vector<std::vector<std::size_t>> polygons;
+
+    struct Item {
+        enum Type { SLASH, INT, FLOAT, KEYWORD, STRING, COMMENT, NEWLINE };
+
+        Item(Type type) :
+            type(type) {}
+
+        Item(Type type, const std::string &text) :
+            type(type), data((void *) new std::string(text)) {}
+
+        Item(Type type, btScalar floatNumber) :
+            type(type), data((void *) new btScalar(floatNumber)) {}
+
+        Item(Type type, int intNumber) :
+            type(type), data((void *) new int(intNumber)) {}
+
+        Item(Item &&item) noexcept :
+            type(item.type), data(item.data)
+        {
+            item.data = nullptr;
+        }
+
+        ~Item()
+        {
+            switch (type) {
+            case INT:
+                delete (int *) data;
+                break;
+            case FLOAT:
+                delete (float *) data;
+                break;
+            case STRING:
+            case KEYWORD:
+            case COMMENT:
+                delete (std::string *) data;
+                break;
+            default:
+                break;
+            }
+        }
+
+        std::string getString()
+        {
+            return *(std::string *) data;
+        }
+
+        const std::string &getString() const
+        {
+            return *(std::string *) data;
+        }
+
+        btScalar getFloat() const
+        {
+            return *(btScalar *) data;
+        }
+
+        int getInt() const
+        {
+            return *(int *) data;
+        }
+
+        std::string show_type() const
+        {
+            switch (type) {
+            case SLASH:
+                return "slash";
+            case INT:
+                return "integer";
+            case FLOAT:
+                return "float";
+            case STRING:
+                return "string";
+            case KEYWORD:
+                return "keyword";
+            case NEWLINE:
+                return "new line or end of file";
+            case COMMENT:
+                return "comment";
+            default:
+                return "";
+            }
+        }
+
+        static std::string show_type(Type t)
+        {
+            switch (t) {
+            case SLASH:
+                return "slash";
+            case INT:
+                return "integer";
+            case FLOAT:
+                return "float";
+            case STRING:
+                return "string";
+            case KEYWORD:
+                return "keyword";
+            case NEWLINE:
+                return "new line or end of file";
+            case COMMENT:
+                return "comment";
+            default:
+                return "";
+            }
+        }
+
+        Type type;
+        void *data = nullptr;
+    };
+    static std::vector<Item> parse(const std::string &filename);
+    void warning(Item::Type expected, Item::Type found);
+};
