b052c51ac1f7070aaa6f3485961a5f38e15563d6
Kotolegokot <gkotolegokot@gmail.com>
2017-01-01 19:27:10 +0300

    Add Vector3 class

---
+++ SERVER/src/util/Vector3.hpp
@@ -0,0 +1,170 @@
+/* This file is part of Plaine.
+ *
+ * Plaine is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Plaine is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Plaine. If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <bullet/LinearMath/btVector3.h>
+
+using Vector3i = btVector3<int>;
+using Vector3f = btVector3<float>;
+
+template <typename Num>
+struct Vector3 {
+    Vector3() = default;
+    Vector3(Num x, Num y, Num z) :
+        x(x), y(y), z(z) {}
+
+    // copy constructors
+    template <typename Num2>
+    Vector3(const Vector3<Num2> &other) :
+        x(other.x), y(other.y), z(other.z) {}
+
+    Vector3(const btVector3 &other) :
+        x(other.x()), y(other.y()), z(other.z()) {}
+
+    // copy assignment operators
+    template <typename Num2>
+    Vector3 &operator =(const Vector3<Num2> &other)
+    {
+        x = other.x;
+        y = other.y;
+        z = other.z;
+
+        return *this;
+    }
+
+    Vector3 &operator =(const btVector3 &other)
+    {
+        x = other.x();
+        y = other.y();
+        z = other.z();
+
+        return *this;
+    }
+
+    operator btVector3() const
+    {
+        return btVector3(x, y, z);
+    }
+
+    template <typename Num2>
+    bool operator ==(const Vector3<Num2> &other) const
+    {
+        return x == other.x &&
+               y == other.y &&
+               z == other.z;
+    }
+
+    template <typename Num2>
+    bool operator !=(const Vector3<Num2> &other) const
+    {
+        return !(*this == other);
+    }
+
+    template <typename Num2>
+    auto operator /(Num2 n) const
+    {
+        return Vector3(x / n, y / n, z / n);
+    }
+
+    template <typename Num2>
+    Vector3 &operator /=(Num2 n)
+    {
+        x /= n;
+        y /= n;
+        z /= n;
+
+        return *this;
+    }
+
+    template <typename Num2>
+    auto operator %(Num2 n) const
+    {
+        return Vector3(x % n, y % n, z % n);
+    }
+
+    template <typename Num2>
+    Vector3 &operator %=(Num2 n)
+    {
+        x %= n;
+        y %= n;
+        z %= n;
+
+        return *this;
+    }
+
+    template <typename Num2>
+    auto operator *(Num2 n) const
+    {
+        return Vector3(x * n, y * n, z * n);
+    }
+
+    template <typename Num2>
+    Vector3 &operator *=(Num2 n)
+    {
+        x *= n;
+        y *= n;
+        z *= n;
+
+        return *this;
+    }
+
+    template <typename Num2>
+    auto operator +(const Vector3<Num2> &other) const
+    {
+        return Vector3(x + other.x, y + other.y, z + other.z);
+    }
+
+    template <typename Num2>
+    Vector3 &operator +=(const Vector3<Num2> &other)
+    {
+        x += other.x;
+        y += other.y;
+        z += other.z;
+
+        return *this;
+    }
+
+    template <typename Num2>
+    auto operator -(const Vector3<Num2> &other) const
+    {
+        return Vector3(x - other.x, y - other.y, z - other.z);
+    }
+
+    template <typename Num2>
+    Vector3 &operator -=(const Vector3<Num2> &other)
+    {
+        x -= other.x;
+        y -= other.y;
+        z -= other.z;
+
+        return *this;
+    }
+
+    template <typename Num2>
+    auto distance(const Vector3<Num2> &other) const
+    {
+        auto dx = x - other.x;
+        auto dy = y - other.y;
+        auto dz = z - other.z;
+
+        return std::sqrt(dx*dx + dy*dy + dz*dz);
+    }
+
+    Num x = 0;
+    Num y = 0;
+    Num z = 0;
+};
